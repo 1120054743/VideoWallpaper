ca041bfe8ddd364acee46a02814f156d5d9a5ce1
lopez <1120054743@qq.com>
2017-12-13 18:52:47 +0800

    更改空判断的处理

--- videowallpaper/src/main/java/com/dingmouren/videowallpaper/VideoWallpaper.java
+++ videowallpaper/src/main/java/com/dingmouren/videowallpaper/VideoWallpaper.java
@@ -6,10 +6,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.res.AssetFileDescriptor;
-import android.content.res.AssetManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.service.wallpaper.WallpaperService;
 import android.text.TextUtils;
 import android.util.Log;
@@ -28,11 +25,13 @@ import static com.dingmouren.videowallpaper.Constant.ACTION_VOICE_SILENCE;
 public class VideoWallpaper extends WallpaperService {
     private static final String TAG = VideoWallpaper.class.getName();
     private static String sVideoPath;
+
     /**
      * 设置静音
+     *
      * @param context
      */
-    public static void setVoiceSilence(Context context){
+    public static void setVoiceSilence(Context context) {
         Intent intent = new Intent(Constant.VIDEO_PARAMS_CONTROL_ACTION);
         intent.putExtra(Constant.ACTION, ACTION_VOICE_SILENCE);
         context.sendBroadcast(intent);
@@ -40,9 +39,10 @@ public class VideoWallpaper extends WallpaperService {
 
     /**
      * 设置有声音
+     *
      * @param context
      */
-    public static void setVoiceNormal(Context context){
+    public static void setVoiceNormal(Context context) {
         Intent intent = new Intent(Constant.VIDEO_PARAMS_CONTROL_ACTION);
         intent.putExtra(Constant.ACTION, ACTION_VOICE_NORMAL);
         context.sendBroadcast(intent);
@@ -50,9 +50,10 @@ public class VideoWallpaper extends WallpaperService {
 
     /**
      * 设置壁纸
+     *
      * @param context
      */
-    public  void setToWallPaper(Context context,String videoPath){
+    public void setToWallPaper(Context context, String videoPath) {
         try {
             context.clearWallpaper();
         } catch (IOException e) {
@@ -60,7 +61,7 @@ public class VideoWallpaper extends WallpaperService {
         }
         sVideoPath = videoPath;
         Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
-        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,new ComponentName(context,VideoWallpaper.class));
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, new ComponentName(context, VideoWallpaper.class));
         context.startActivity(intent);
     }
 
@@ -70,7 +71,7 @@ public class VideoWallpaper extends WallpaperService {
         return new VideoWallpagerEngine();
     }
 
-    class VideoWallpagerEngine extends Engine{
+    class VideoWallpagerEngine extends Engine {
         private MediaPlayer mMediaPlayer;
         private BroadcastReceiver mVideoVoiceControlReceiver;
 
@@ -79,7 +80,7 @@ public class VideoWallpaper extends WallpaperService {
             super.onCreate(surfaceHolder);
             IntentFilter intentFilter = new IntentFilter(Constant.VIDEO_PARAMS_CONTROL_ACTION);
             mVideoVoiceControlReceiver = new VideoVoiceControlReceiver();
-            registerReceiver(mVideoVoiceControlReceiver,intentFilter);
+            registerReceiver(mVideoVoiceControlReceiver, intentFilter);
         }
 
         @Override
@@ -90,18 +91,20 @@ public class VideoWallpaper extends WallpaperService {
 
         @Override
         public void onVisibilityChanged(boolean visible) {
-            if (visible){
+            if (visible) {
                 mMediaPlayer.start();
-            }else {
-              mMediaPlayer.pause();
+            } else {
+                mMediaPlayer.pause();
             }
         }
+
         @Override
         public void onSurfaceCreated(SurfaceHolder holder) {
             super.onSurfaceCreated(holder);
             if (TextUtils.isEmpty(sVideoPath)) {
-                throw  new NullPointerException("videoPath must not be null ");
-            }else {
+                //throw  new NullPointerException("videoPath must not be null ");
+                Log.d("==w", "videoPath为空: " + sVideoPath);
+            } else {
                 mMediaPlayer = new MediaPlayer();
                 mMediaPlayer.setSurface(holder.getSurface());
 
@@ -127,28 +130,28 @@ public class VideoWallpaper extends WallpaperService {
         @Override
         public void onSurfaceDestroyed(SurfaceHolder holder) {
             super.onSurfaceDestroyed(holder);
-            if (mMediaPlayer != null){
+            if (mMediaPlayer != null) {
                 mMediaPlayer.release();
                 mMediaPlayer = null;
             }
         }
-        class VideoVoiceControlReceiver extends BroadcastReceiver{
+
+        class VideoVoiceControlReceiver extends BroadcastReceiver {
 
             @Override
             public void onReceive(Context context, Intent intent) {
-                int action = intent.getIntExtra(Constant.ACTION,-1);
-                switch (action){
+                int action = intent.getIntExtra(Constant.ACTION, -1);
+                switch (action) {
                     case ACTION_VOICE_NORMAL:
-                        mMediaPlayer.setVolume(1.0f,1.0f);
+                        mMediaPlayer.setVolume(1.0f, 1.0f);
                         break;
                     case ACTION_VOICE_SILENCE:
-                        mMediaPlayer.setVolume(0,0);
+                        mMediaPlayer.setVolume(0, 0);
                         break;
                 }
             }
         }
     }
-
 
 
 }
